b961a537755092cf46d0ea31818b38c5454fabac
pyaare-10 <pritambachar132@gmail.com>
2026-04-15 12:10:49 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    siteName: "ROYAL WINNER 777",
+    upiID: "8183000000@jio", 
+    qrLink: "https://i.postimg.cc/FH2Jb2P9/Account-QRCode-Jio-Payments-Bank-8183-DARK-THEME-1.jpg",
+    minDeposit: 300,
+    supportLink: "https://t.me/YOUR_TELEGRAM_USERNAME",
+    adminPass: "admin123" 
+};
